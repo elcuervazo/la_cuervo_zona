93ad3edd1d8074b92c8514d690ef83af212ac9c5
Camilo Cuervo <167337666+elcuervazo@users.noreply.github.com>
2024-05-02 08:52:03 -0500

    Delete estructura_selectiva directory

--- estructura_selectiva/ejercicio1.py
+++
@@ -1,5 +0,0 @@
-note1=float(input("Escriba la primer nota: "))
-note2=float(input("Escriba la segunda nota: "))
-note3=float(input("Escriba la tercer nota: "))
-prom= (note1+note2+note3)/3
-print ("Aprobado mi rey, paso con",prom) if prom > 70  else print("paila mi rey, perdio con",prom,)
